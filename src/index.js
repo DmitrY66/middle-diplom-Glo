f01fbc83133acb47bf8de841416eebe7210c1eca
Dmitri Gorun <iii448@mail.ru>
2021-09-15 03:13:19 +0500

    task2_sliderCarouselService_2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import topModalOpenClose from "./modules/1_topModalOpenClose";
 import classSliderCarousel from "./modules/2_sliderCarouselBenefitsUser";
+import classCarousel_service from "./modules/3_sliderCarousel_service";
 
 topModalOpenClose();
 classSliderCarousel();
+classCarousel_service();
