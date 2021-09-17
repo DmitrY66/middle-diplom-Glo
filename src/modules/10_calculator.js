07d894a8979cf1845c1e8d9d033137007a941b74
Dmitri Gorun <iii448@mail.ru>
2021-09-17 19:51:28 +0500

    task10_calculator_1

---
+++ src/modules/10_calculator.js
@@ -0,0 +1,46 @@
+const calculator = () => {
+  const calcBlock = document.querySelector('#calc');
+
+  try {
+    calcBlock.addEventListener('input', (e) => {
+      let target = e.target;
+
+      if (target.matches('input')) {
+        target.value = target.value.replace(/\D/g, '');
+      }
+    });
+  } catch (err) { }
+
+  const calc = (price = 100) => {
+    const calcType = document.querySelector('#calc-type');
+    const calcTypeMaterial = document.querySelector('#calc-type-material');
+    const calcSquare = document.querySelector('#calc-input');
+    const calcTotal = document.querySelector('#calc-total');
+
+    const countSum = () => {
+      let priceTotal = 0;
+
+      if (calcSquare.value != '' && calcType.value != '--' && calcTypeMaterial.value != '--') {
+        priceTotal = Math.round((+calcSquare.value * +calcType.value * +calcTypeMaterial.value) * price);
+        calcTotal.value = priceTotal;
+      } else {
+        calcTotal.placeholder = 'заполните все поля';
+        calcTotal.value = 'заполните все поля';
+      }
+    };
+
+    try {
+      calcBlock.addEventListener('change', (e) => {
+        const target = e.target;
+
+        if (target.matches('select') || target.matches('input')) {
+          countSum();
+        }
+      });
+    } catch (err) { }
+  };
+
+  calc();
+};
+
+export default calculator;
